33ebe6ec09477f862b1f002832736ba141c5d66d
ShoutaKawasumi <kawasumi@forum8.co.jp>
2017-03-16 17:19:47 +0900

    TEST5

--- TEST.C
+++ TEST.C
@@ -1,13 +1,30 @@
 #include<stdio.h>
 
+int Kokugo,Shakai,Rika;
+int Goukei,Heikin;
+
+void Heikinkeisan ( int, int);
+
 main()
 {
-	int Kokugo,Shakai,Rika;
-    int Goukei,Heikin;
+	
 
     Kokugo = 90;
 	Shakai = 90;
 	Rika = 90;
 	
+	Goukei = Kokugo + Shakai + Rika;
+	
+	if (Goukei > 0) {
+		Heikinkeisan (Goukei, Heikin);
+	}
 
 }
+
+void Heikinkeisan(
+	int Goukei,
+	int Heikin)
+{
+	Heikin = Goukei / 3;
+}
+	
