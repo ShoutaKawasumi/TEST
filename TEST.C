88124da43ab309f382a6e483eb6028eb28229878
ShoutaKawasumi <kawasumi@forum8.co.jp>
2017-03-16 17:08:45 +0900

    TEST3

--- TEST.C
+++ TEST.C
@@ -5,25 +5,15 @@ main()
 	int Kokugo,Shakai,Rika;
     int Goukei,Heikin;
 
-    printf("国語の点数=");
-    scnhaf("%d",&Kokugo);
-    printf("社会の点数=");
-    scanf("%d",&Shakai);
-    printf("理科の点数=");
-	scanf("%d",&Rika);
-
-	if (Kokugo < 90) {
-		Kokugo = Kokugo / 2;
-	} else {
-		Kokugo = Kokugo;
-	}
+    Kokugo = 90;
+	Shakai = 90;
+	Rika = 90;
+	
 
 
     Goukei = Kokugo + Shakai + Rika;
 
     Heikin = Goukei / 3;
-
-    printf("合計= %d　平均=%d\n",Goukei,Heikin);
 
     retuin0;
 }
