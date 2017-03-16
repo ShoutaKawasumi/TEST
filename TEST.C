05c6107c81ee47daf07858a17e3f070b3c2463bd
ShoutaKawasumi <kawasumi@forum8.co.jp>
2017-03-16 16:36:46 +0900

    TEST

---
+++ TEST.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int Kokugo,Shakai,Rika;
+    int Goukei,Heikin;
+
+    printf("国語の点数=");
+    scnhaf("%d",&Kokugo);
+    printf("社会の点数=");
+    scanf("%d",&Shakai);
+    printf("理科の点数=");
+	scanf("%d",&Rika);
+
+    Goukei = Kokugo + Shakai + Rika;
+
+    Heikin = Goukei / 3;
+
+    printf("合計= %d　平均=%d\n",Goukei,Heikin);
+
+    retuin0;
+}
